e88a3c731aa0908fa0555f8d64ac3ebde0f755ca
Shruthi Dakur Rudraiah <sdakurrudraiah@seattleu.edu>
2022-01-08 20:49:59 -0800

    My first R script

--- my_first_R.R
+++ my_first_R.R
@@ -6,15 +6,18 @@ summary(Alumni)         #Computes summary statistics on alumni Dataframe(minimum
 summary(subset(Alumni,select=c(sfratio,alumnigivingrate))) 
 summary(subset(Alumni,subset=sfratio<10,select=c(sfratio,alumnigivingrate))) 
 nfhs<-read_dta("IAHR52FL.dta")      #reads IAHR52FL.dta and generates nfhs Dataframe
+
 new_df <- nfhs%>%                   #read new_df Dataframe                 
   select(hhid:hv208)%>%
   rename(survey_month=hv006)%>%
   filter(survey_month==1)%>%
   mutate(rural=hv025==2)
+
 ggplot(data=nfhs,                 #Plot graph for count of Number of household members
        mapping=aes(x=hv009),binwidth=1)+
   geom_histogram()+
   xlab("Number of household members")
+
 new_df %>%                       #Get the count of households rural and urban
   group_by(rural)%>%
   count()
